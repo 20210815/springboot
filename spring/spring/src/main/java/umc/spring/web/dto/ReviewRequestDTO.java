0eb7596f3a56c471fea9cda718022d164bf48d6d
이규민 <90364652+20210815@users.noreply.github.com>
2024-04-13 22:20:54 +0900

    [FEAT] REVIEW 달기 기능 추가

---
+++ spring/spring/src/main/java/umc/spring/web/dto/ReviewRequestDTO.java
@@ -0,0 +1,15 @@
+package umc.spring.web.dto;
+
+import lombok.Getter;
+
+public class ReviewRequestDTO {
+
+    //리뷰를 만들겠다.
+    @Getter
+    public static class CreateReviewDTO {
+        String title;
+        Float score;
+        String body;
+        Long memberId;
+    }
+}
