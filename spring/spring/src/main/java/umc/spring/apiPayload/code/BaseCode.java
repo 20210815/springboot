adbefc0852b5f42bb92ebcd51bd00333be63f8ab
이규민 <90364652+20210815@users.noreply.github.com>
2024-04-13 17:54:49 +0900

    [FEAT]: API 응답 통일

---
+++ spring/spring/src/main/java/umc/spring/apiPayload/code/BaseCode.java
@@ -0,0 +1,9 @@
+package umc.spring.apiPayload.code;
+
+import java.awt.desktop.UserSessionEvent;
+
+public interface BaseCode {
+    public ReasonDTO getReason();
+
+    public ReasonDTO getReasonHttpStatus();
+}
