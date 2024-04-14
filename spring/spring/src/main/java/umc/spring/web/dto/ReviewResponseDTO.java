e138a8cb51911c26aef41c953828fc0c50ba47b9
이규민 <90364652+20210815@users.noreply.github.com>
2024-04-14 22:12:33 +0900

    [FEAT] 특정 가게 리뷰 목록 확인하기

--- spring/spring/src/main/java/umc/spring/web/dto/ReviewResponseDTO.java
+++ spring/spring/src/main/java/umc/spring/web/dto/ReviewResponseDTO.java
@@ -1,11 +1,10 @@
 package umc.spring.web.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class ReviewResponseDTO {
 
@@ -17,4 +16,31 @@ public class ReviewResponseDTO {
         Long reviewId;
         LocalDateTime createdAt;
     }
+
+    @Builder
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class ReviewPreviewListDTO {
+        List<ReviewPreviewDTO> reviewList;
+        Integer listSize;
+        Integer totalPage;
+        Long totalElements;
+        Boolean isFirst;
+        Boolean isLast;
+    }
+
+
+    @Builder
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class ReviewPreviewDTO{
+        String nickName;
+        Float score;
+        String body;
+        LocalDate createdAt;
+    }
+
+
 }
